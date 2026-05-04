9ccc326704945ff9f1c5424a86ff4d7ae30da759
Kayla Man <moonyuet2008@hotmail.com>
2026-05-04 17:09:27 +0800

    align with the parameter values across the host

--- client/ayon_deadline_cloud/plugins/publish/collect_deadline_cloud_job_data.py
+++ client/ayon_deadline_cloud/plugins/publish/collect_deadline_cloud_job_data.py
@@ -68,7 +68,7 @@ class CollectDeadlineCloudJobData(
         ayon_settings = instance.context.data.get("project_settings", {})
         dc_settings = ayon_settings.get("deadline_cloud", {})
         submitter_bg = get_submitter_bridge(
-            host_name=instance.context.data["hostName"],
+            host_name=get_current_host_name,
             instance=instance,
         )
         settings = submitter_bg.submitter_settings
@@ -141,7 +141,7 @@ class CollectDeadlineCloudJobData(
 
         """
         submitter_bg = get_submitter_bridge(
-            host_name=instance.context.data["host_name"],
+            host_name=get_current_host_name(),
             instance=instance,
         )
         job_template = submitter_bg.get_job_template_for_submission(settings)
