cb610407a2d199af025853d72271326d7ded8614
Kayla Man <moonyuet2008@hotmail.com>
2026-05-06 21:51:47 +0800

    add conditon check if there is any known asset paths

--- client/ayon_deadline_cloud/plugins/publish/submit_to_deadline_cloud.py
+++ client/ayon_deadline_cloud/plugins/publish/submit_to_deadline_cloud.py
@@ -76,7 +76,7 @@ class SubmitToDeadlineCloud(pyblish.api.InstancePlugin):
                 "job_bundle_dir": temp_dir,
                 "print_function_callback": self.log.info,
             }
-            if known_paths:
+            if instance.context.data["hostName"] == "maya":
                 kwargs["known_asset_paths"] = known_paths
                 kwargs["interactive_confirmation_callback"] = (
                     lambda _msg, _default: True
