d44d6a0356c4c3079c673c4080a013f3032253c9
Kayla Man <moonyuet2008@hotmail.com>
2026-05-12 16:32:37 +0800

    ruff fix

--- client/ayon_deadline_cloud/plugins/create/houdini/create_job.py
+++ client/ayon_deadline_cloud/plugins/create/houdini/create_job.py
@@ -10,13 +10,12 @@ from ayon_core.lib import (
     NumberDef,
     TextDef,
 )
-from ayon_core.pipeline import CreatedInstance
 from ayon_deadline_cloud.api.submitter_bridge import HoudiniSetting
 from ayon_houdini.api import plugin
-from ayon_houdini.api.lib import read
 
 if TYPE_CHECKING:
     import pyblish.api
+    from ayon_core.pipeline import CreatedInstance
 
 
 class CreateDeadlineCloudJob(plugin.HoudiniCreator):
